e6b829134675b6f1d74b698b8f8bacc5ac87a238
Gordon Cumming <dynaphoneimages@hotmail.com>
2017-12-20 13:13:53 +0000

    modified 2 test and e2e files

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,7 +9,7 @@ describe('bynd-tst-dec17 App', () => {
 
     it('should display welcome message', () => {
         page.navigateTo();
-        expect(page.getParagraphText()).toEqual('Welcome to app!');
+        expect(page.getParagraphText()).toEqual('Beyond Test Dec 2017 app!');
     });
 
 });
